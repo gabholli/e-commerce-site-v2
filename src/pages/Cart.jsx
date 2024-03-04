c3e530b1398c833f1653188ed66630f05c75ea10
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-03-03 22:11:31 -0800

    Updated project title

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CartItems from '../components/CartItems'
 
 export default function Cart() {
+
     return (
         <div>
             <CartItems />
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -29,7 +29,7 @@ export default function ProductDetail() {
             }
         }
         fetchProductDetail()
-    }, [])
+    }, [productId])
 
     if (loading) {
         return (
